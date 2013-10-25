f4b1e2e0b25ae30e1ca4eb6d750024f3e460c162
Scott Burns <scott.s.burns@gmail.com>
2013-10-25 09:02:45 -0500

    Add process admin class

--- redcaptainhook/apps/workflow/admin.py
+++ redcaptainhook/apps/workflow/admin.py
@@ -9,7 +9,10 @@ class ProjectAdmin(admin.ModelAdmin):
 class TriggerAdmin(admin.ModelAdmin):
     list_display = ('id', 'status', 'form', 'event', 'dag', 'active', 'project', )
 
+class ProcessAdmin(admin.ModelAdmin):
+    list_display = ('name','qname','fname','active', )
+
 
 admin.site.register(Trigger, TriggerAdmin)
 admin.site.register(Project, ProjectAdmin)
-admin.site.register(Process)
+admin.site.register(Process, ProcessAdmin)
